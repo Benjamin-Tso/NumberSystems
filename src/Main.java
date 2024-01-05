688dbe8be525fc665aa115e6a70d924d76030560
unknown <BT_4N2_15@BKTECH.LOCAL>
2024-01-05 13:59:10 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        NumberSystem.convert("4B",16,10);
+    }
+}
---
+++ src/NumberSystem.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+public class NumberSystem {
+    private static final char[] digits = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    public static String convert(String num, int base, int targetBase){
+        String out = "";
+        char[] baseChars = new char[base];
+        for(int i = 0; i<base;i++){
+            baseChars[i]=digits[i];
+        }
+        char[] targetBaseChars = new char[targetBase];
+        for(int i = 0; i<targetBase;i++){
+            targetBaseChars[i]=digits[i];
+        }
+        int numBase10 = convertToBase10(num,base);
+        System.out.println(numBase10);
+        return out;
+    }
+    private static int convertToBase10(String num, int base){
+        String rNum = "";
+        for(int i = 0; i<num.length();i++){
+            rNum = 
+        }
+        int out = 0;
+        for(int i = num.length()-1; i>=0;i--){
+            char digit = num.charAt(i);
+            for(int b = 0; b<digits.length;b++){
+                if(digits[b]==digit){
+                    out+=b*Math.pow(base,i);
+                    break;
+                }
+            }
+        }
+        return out;
+    }
+}
